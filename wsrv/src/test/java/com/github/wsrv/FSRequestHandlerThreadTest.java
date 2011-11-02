2a12c0f25ceacefec338f75f81c31b4845f1804c
tteofili <tommaso.teofili@gmail.com>
2011-11-02 17:54:42 +0100

    added base test for FSRequestHandlerThread

---
+++ wsrv/src/test/java/com/github/wsrv/FSRequestHandlerThreadTest.java
@@ -0,0 +1,22 @@
+package com.github.wsrv;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
+
+/**
+ * @author tommaso
+ */
+public class FSRequestHandlerThreadTest {
+  @Test
+  public void testResourceFetch() {
+    try {
+      FSRequestHandlerThread fsRequestHandlerThreadTest = new FSRequestHandlerThread("./");
+      WSRVResource resource = fsRequestHandlerThreadTest.call();
+      assertNotNull(resource);
+    } catch (Exception e) {
+      fail(e.getLocalizedMessage());
+    }
+  }
+}
