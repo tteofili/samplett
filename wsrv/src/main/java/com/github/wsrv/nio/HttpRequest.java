5eb375a21f180cc5a271c604e2bfdf44ad02001e
tteofili <tommaso.teofili@gmail.com>
2011-11-08 22:22:55 +0100

    basic resource retrieve added in SocketHandler

--- wsrv/src/main/java/com/github/wsrv/nio/HttpRequest.java
+++ wsrv/src/main/java/com/github/wsrv/nio/HttpRequest.java
@@ -4,4 +4,13 @@ package com.github.wsrv.nio;
  * @author tommaso
  */
 public class HttpRequest {
+  private String path;
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
 }
--- wsrv/src/main/java/com/github/wsrv/nio/SocketHandler.java
+++ wsrv/src/main/java/com/github/wsrv/nio/SocketHandler.java
@@ -3,7 +3,11 @@ package com.github.wsrv.nio;
 import com.github.wsrv.cache.WSRVResourceCache;
 import com.github.wsrv.cache.WSRVResourceCacheProvider;
 import com.github.wsrv.jetty.WSRVResource;
+import org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.Callable;
@@ -31,6 +35,31 @@ public class SocketHandler implements Callable<Object> {
     WSRVResource resource = cache.get(httpRequest);
     if (resource == null) {
       // retrieve the resource
+      final byte[] byteStream;
+      String path = httpRequest.getPath();
+      File file = new File(path);
+      if (!file.exists()) {
+        throw new FileNotFoundException(new StringBuilder(path).append(" not found").toString());
+      }
+      if (file.isFile()) {
+        try {
+          byteStream = IOUtils.toByteArray(new FileInputStream(file));
+        } catch (Exception e) {
+          throw new FileNotFoundException(e.getLocalizedMessage());
+        }
+      } else {
+        StringBuilder sb = new StringBuilder();
+        for (File f : file.listFiles()) {
+          sb.append(f.getName()).append("\n");
+        }
+        byteStream = sb.toString().getBytes();
+      }
+      return new WSRVResource() {
+        @Override
+        public byte[] getBytes() {
+          return byteStream;
+        }
+      };
     }
     byteBuffer.clear(); //make buffer ready for writing
     HttpResponse httpResponse = new HttpResponse();
