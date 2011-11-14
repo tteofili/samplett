a02753916d3632a265bbaa24241c3da7c37d1100
tteofili <tommaso.teofili@gmail.com>
2011-11-14 12:30:17 +0100

    enhanced debugging message on webserverrunner and added stop() call on exception being catched

--- wsrv/src/main/java/com/github/wsrv/nio/WebServerRunner.java
+++ wsrv/src/main/java/com/github/wsrv/nio/WebServerRunner.java
@@ -1,22 +1,28 @@
 package com.github.wsrv.nio;
 
+import java.io.IOException;
+
 /**
  * @author tommaso
  */
 public class WebServerRunner {
   public static void main(String[] args) {
+    NIOWebServer server = new NIOWebServer();
     try {
       Integer poolSize = Integer.valueOf(args[0]);
       System.out.println("thread number set to: " + poolSize);
       String root = args[1];
       System.out.println("root is : " + root);
-      NIOWebServer server = new NIOWebServer();
       ServerConfiguration serverConfiguration = new ServerConfiguration(poolSize, root);
       server.init(serverConfiguration);
       server.run();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println("Could not listen on port: 4444");
+      System.err.println("Could not start the server due to " + e.getLocalizedMessage());
+      try {
+        server.stop();
+      } catch (IOException e1) {
+        // do nothing
+      }
       System.exit(-1);
     }
   }
