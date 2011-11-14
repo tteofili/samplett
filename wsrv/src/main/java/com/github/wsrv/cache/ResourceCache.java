a45f05fbeee4a3fef1ddf7502259eda4faee2ac6
tteofili <tommaso.teofili@gmail.com>
2011-11-14 10:40:09 +0100

    resource cache must return resource, not a wsrvresource

--- wsrv/src/main/java/com/github/wsrv/cache/ResourceCache.java
+++ wsrv/src/main/java/com/github/wsrv/cache/ResourceCache.java
@@ -3,8 +3,8 @@ package com.github.wsrv.cache;
 /**
  * @author tommaso
  */
-public interface ResourceCache<K, WSRVResource> {
-  public WSRVResource get(K key);
+public interface ResourceCache<K, Resource> {
+  public Resource get(K key);
 
-  public WSRVResource put(K key, WSRVResource value);
+  public Resource put(K key, Resource value);
 }
