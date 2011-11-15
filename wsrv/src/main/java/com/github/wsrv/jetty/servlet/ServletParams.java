11676728d14844ba34923df1b858898f66066a50
tteofili <tommaso.teofili@gmail.com>
2011-11-15 16:42:15 +0100

    removed jetty package

--- wsrv/src/main/java/com/github/wsrv/jetty/servlet/ServletParams.java
+++
@@ -1,9 +0,0 @@
-package com.github.wsrv.jetty.servlet;
-
-/**
- * @author tommaso
- */
-public class ServletParams {
-  static final String ROOT_PARAMETER = "repo-root";
-  static final String POOL_SIZE_PARAMETER = "pool-size";
-}
