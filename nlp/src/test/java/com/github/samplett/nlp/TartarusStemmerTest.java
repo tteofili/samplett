61daa8f49879632335ca99e052c24792b6283c80
teofili <70m450>
2012-04-02 22:41:31 +0200

    added the nlp module

---
+++ nlp/src/test/java/com/github/samplett/nlp/TartarusStemmerTest.java
@@ -0,0 +1,14 @@
+package com.github.samplett.nlp;
+
+import org.junit.Test;
+
+/**
+ * Add javadoc here
+ */
+public class TartarusStemmerTest {
+
+  @Test
+  public void testBasicExecution() throws Exception {
+
+  }
+}
