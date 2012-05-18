98070d8898f17f3c7b00f35a45d3b6a7ff0b5a7f
Tommaso Teofili <tommaso.teofili@gmail.com>
2012-05-18 13:24:01 +0200

    adding the topMatches method to Recommend

--- skaluh/src/main/scala/com/github/samplett/skaluh/Recommend.scala
+++ skaluh/src/main/scala/com/github/samplett/skaluh/Recommend.scala
@@ -9,6 +9,9 @@ class Recommend {
   val sampleSet = Map(
     "john" -> Map("back to the future" -> 10d, "the social network" -> 9.5d, "point break" -> 4.5d, "bomber" -> 9.5d, "sherlock holmes" -> 8d),
     "paula" -> Map("harry potter and the prisoner of azkaban" -> 7d, "back to the future" -> 3d, "the social network" -> 8d, "bomber" -> 2d, "sherlock holems" -> 8.5d),
+    "jim" -> Map("harry potter and the prisoner of azkaban" -> 4d, "back to the future" -> 5d, "the social network" -> 5d, "bomber" -> 8d, "sherlock holems" -> 1.5d),
+    "tom" -> Map("harry potter and the prisoner of azkaban" -> 9d, "back to the future" -> 4d, "the social network" -> 9d, "bomber" -> 1d, "sherlock holems" -> 5d),
+    "bob" -> Map("harry potter and the prisoner of azkaban" -> 1d, "back to the future" -> 3d, "the social network" -> 8d, "bomber" -> 2d, "sherlock holems" -> 9d),
     "timothy" -> Map("harry potter and the prisoner of azkaban" -> 7d, "back to the future" -> 3d, "the social network" -> 5.5d, "bomber" -> 2d, "sherlock holems" -> 9.5d)
   )
 
@@ -64,6 +67,16 @@ class Recommend {
     if (den == 0) return 0
 
     return num / den
+  }
+
+
+  def topMatches(prefs: Map[String, Map[String, Double]], person: String, n: Int, sim: (Map[String, Map[String, Double]],
+    String, String) => Double): Set[String] = {
+    var scores = new scala.collection.mutable.HashMap[String, Double];
+    for (other <- prefs if other._1 != person)
+      scores += other._1 -> sim(prefs, person, other._1);
+    var sortedList = scores.toList sortBy (_._2)
+    return sortedList.reverse.slice(0, n).toMap[String, Double].keySet;
   }
 
 }
--- skaluh/src/test/scala/com/github/samplett/skaluh/RecommendTest.scala
+++ skaluh/src/test/scala/com/github/samplett/skaluh/RecommendTest.scala
@@ -12,28 +12,42 @@ class RecommendTest {
   def sim_distanceTest() {
     val r = new Recommend();
     var simDistance = r.sim_distance(r.sampleSet, "paula", "john")
-    Console.println("paula and john similarity is : " + simDistance)
+    Console.println("paula and john distance based similarity is : " + simDistance)
     // john and paula are quite far
     assertTrue(simDistance < 0.1);
 
     // paula and timothy are less far
     simDistance = r.sim_distance(r.sampleSet, "paula", "timothy")
-    Console.println("paula and timothy similarity is : " + simDistance)
+    Console.println("paula and timothy distance based similarity is : " + simDistance)
     assertTrue(simDistance > 0.2);
   }
 
   @Test
   def sim_pearsonTest() {
     val r = new Recommend();
-    var simDistance = r.sim_pearson(r.sampleSet, "paula", "john")
-    Console.println("paula and john similarity is : " + simDistance)
+    var simPearson = r.sim_pearson(r.sampleSet, "paula", "john")
+    Console.println("paula and john pearson correlation based similarity is : " + simPearson)
     // john and paula are quite far
-    assertTrue(simDistance < 0.1);
+    assertTrue(simPearson < 0.1);
 
     // paula and timothy are less far
-    simDistance = r.sim_pearson(r.sampleSet, "paula", "timothy")
-    Console.println("paula and timothy similarity is : " + simDistance)
-    assertTrue(simDistance > 0.2);
+    simPearson = r.sim_pearson(r.sampleSet, "paula", "timothy")
+    Console.println("paula and timothy pearson correlation based similarity is : " + simPearson)
+    assertTrue(simPearson > 0.2);
+  }
+
+  @Test
+  def topMatchesTestWithDistance() {
+    val r = new Recommend();
+    var sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_distance)
+    Console.println(sortedList)
+  }
+
+  @Test
+  def topMatchesTestWithPearson() {
+    val r = new Recommend();
+    var sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_pearson)
+    Console.println(sortedList)
   }
 
 }
