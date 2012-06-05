6b58d9be85b140dd5ed55ae64556ee696b2efa65
Tommaso Teofili <tommaso.teofili@gmail.com>
2012-06-05 11:50:37 +0200

    fixed style

--- nlp/src/main/java/com/github/samplett/nlp/util/NGramUtils.java
+++ nlp/src/main/java/com/github/samplett/nlp/util/NGramUtils.java
@@ -7,51 +7,52 @@ import java.util.Collection;
  */
 public class NGramUtils {
 
-    private static Double count(String sequentWord, String precedingWord, Collection<String[]> set) {
-        Double result = 0d;
-        boolean foundPreceding = false;
-        for (String[] sentence : set) {
-            for (String w : sentence) {
-                if (precedingWord.equals(w)) {
-                    foundPreceding = true;
-                    continue;
-                }
-                if (foundPreceding && sequentWord.equals(w)) {
-                    foundPreceding = false;
-                    result++;
-                } else
-                    foundPreceding = false;
-            }
+  private static Double count(String sequentWord, String precedingWord, Collection<String[]> set) {
+    Double result = 0d;
+    boolean foundPreceding = false;
+    for (String[] sentence : set) {
+      for (String w : sentence) {
+        if (precedingWord.equals(w)) {
+          foundPreceding = true;
+          continue;
         }
-        return result;
-    }
-
-    private static Double count(String word, Collection<String[]> set) {
-        Double result = 0d;
-        for (String[] sentence : set) {
-            for (String w : sentence) {
-                if (word.equals(w))
-                    result++;
-            }
+        if (foundPreceding && sequentWord.equals(w)) {
+          foundPreceding = false;
+          result++;
         }
-        return result;
+        else
+          foundPreceding = false;
+      }
     }
+    return result;
+  }
 
-    public static Double calculateLaplaceSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
-        return (count(sequentWord, precedingWord, set) + k) / (count(precedingWord, set) + k * set.size());
+  private static Double count(String word, Collection<String[]> set) {
+    Double result = 0d;
+    for (String[] sentence : set) {
+      for (String w : sentence) {
+        if (word.equals(w))
+          result++;
+      }
     }
+    return result;
+  }
 
-    public static Double calculateBigramProbability(String sequentWord, String precedingWord, Collection<String[]> set) {
-        return count(sequentWord, precedingWord, set) / count(precedingWord, set);
-    }
+  public static Double calculateLaplaceSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
+    return (count(sequentWord, precedingWord, set) + k) / (count(precedingWord, set) + k * set.size());
+  }
 
-    public static Double calculateBigramPriorSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
-        return (count(sequentWord, precedingWord, set) + k * calculateProbability(sequentWord, set)) / (count(precedingWord, set) + k * set.size());
-    }
+  public static Double calculateBigramProbability(String sequentWord, String precedingWord, Collection<String[]> set) {
+    return count(sequentWord, precedingWord, set)/ count(precedingWord, set);
+  }
 
-    private static Double calculateProbability(String word, Collection<String[]> set) {
-        return count(word, set) / set.size();
-    }
+  public static Double calculateBigramPriorSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
+    return (count(sequentWord, precedingWord, set) + k * calculateProbability(sequentWord, set)) / (count(precedingWord, set) + k * set.size());
+  }
+
+  private static Double calculateProbability(String word, Collection<String[]> set) {
+    return count(word, set) / set.size();
+  }
 
 
 }
--- nlp/src/test/java/com/github/samplett/nlp/util/NGramUtilsTest.java
+++ nlp/src/test/java/com/github/samplett/nlp/util/NGramUtilsTest.java
@@ -12,19 +12,19 @@ import static org.junit.Assert.assertTrue;
  * Add javadoc here
  */
 public class NGramUtilsTest {
-    @Test
-    public void testBigram() {
-        Collection<String[]> set = new LinkedList<String[]>();
-        set.add(new String[]{"<s>", "I", "am", "Sam", "</s>"});
-        set.add(new String[]{"<s>", "Sam", "I", "am", "</s>"});
-        set.add(new String[]{"<s>", "I", "do", "not", "like", "green", "eggs", "and", "ham", "</s>"});
-        set.add(new String[]{});
-        Double d = NGramUtils.calculateBigramProbability("I", "<s>", set);
-        assertTrue(d > 0);
-        assertEquals(Double.valueOf(0.6666666666666666d), d);
-        d = NGramUtils.calculateBigramProbability("</s>", "Sam", set);
-        assertEquals(Double.valueOf(0.5d), d);
-        d = NGramUtils.calculateBigramProbability("Sam", "<s>", set);
-        assertEquals(Double.valueOf(0.3333333333333333d), d);
-    }
+  @Test
+  public void testBigram() {
+    Collection<String[]> set = new LinkedList<String[]>();
+    set.add(new String[]{"<s>","I","am","Sam","</s>"});
+    set.add(new String[]{"<s>","Sam","I","am","</s>"});
+    set.add(new String[]{"<s>","I","do","not","like","green","eggs","and","ham","</s>"});
+    set.add(new String[]{});
+    Double d = NGramUtils.calculateBigramProbability("I","<s>",set);
+    assertTrue(d>0);
+    assertEquals(Double.valueOf(0.6666666666666666d),d);
+    d = NGramUtils.calculateBigramProbability("</s>","Sam",set);
+    assertEquals(Double.valueOf(0.5d),d);
+    d = NGramUtils.calculateBigramProbability("Sam","<s>",set);
+    assertEquals(Double.valueOf(0.3333333333333333d),d);
+  }
 }
