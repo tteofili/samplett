fc3527683782880330d182cb720d11cc85f26f58
Tommaso Teofili <tommaso.teofili@gmail.com>
2012-06-05 11:49:42 +0200

    fixed NGramUtils probability algorithms, changed type from long to double and added test

--- nlp/src/main/java/com/github/samplett/nlp/util/NGramUtils.java
+++ nlp/src/main/java/com/github/samplett/nlp/util/NGramUtils.java
@@ -7,46 +7,51 @@ import java.util.Collection;
  */
 public class NGramUtils {
 
-  public static Long calculateProbability(String word, String precedingWord, Collection<String> set) {
-    return count(word, precedingWord, set) / count(word, set);
-  }
-
-  private static Long count(String word, String precedingWord, Collection<String> set) {
-    Long result = 0l;
-    boolean foundPreceding = false;
-    for (String w : set) {
-      if (precedingWord.equals(w)) {
-        foundPreceding = true;
-      }
-      if (foundPreceding && word.equals(w)) {
-        foundPreceding = false;
-        result++;
-      } else
-        foundPreceding = false;
+    private static Double count(String sequentWord, String precedingWord, Collection<String[]> set) {
+        Double result = 0d;
+        boolean foundPreceding = false;
+        for (String[] sentence : set) {
+            for (String w : sentence) {
+                if (precedingWord.equals(w)) {
+                    foundPreceding = true;
+                    continue;
+                }
+                if (foundPreceding && sequentWord.equals(w)) {
+                    foundPreceding = false;
+                    result++;
+                } else
+                    foundPreceding = false;
+            }
+        }
+        return result;
     }
-    return result;
-  }
 
-  private static Long count(String word, Collection<String> set) {
-    Long result = 0l;
-    for (String w : set) {
-      if (word.equals(w))
-        result++;
+    private static Double count(String word, Collection<String[]> set) {
+        Double result = 0d;
+        for (String[] sentence : set) {
+            for (String w : sentence) {
+                if (word.equals(w))
+                    result++;
+            }
+        }
+        return result;
     }
-    return result;
-  }
 
-  public static Long calculateLaplaceSmoothingProbability(String word, String precedingWord, Collection<String> set, Long k) {
-    return (count(word, precedingWord, set) + k) / (count(word, set) + k * set.size());
-  }
+    public static Double calculateLaplaceSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
+        return (count(sequentWord, precedingWord, set) + k) / (count(precedingWord, set) + k * set.size());
+    }
 
-  public static Long calculateUnigramPriorSmoothingProbability(String word, String precedingWord, Collection<String> set, Long k) {
-    return (count(word, precedingWord, set) + k * calculateProbability(word, set)) / (count(word, set) + k * set.size());
-  }
+    public static Double calculateBigramProbability(String sequentWord, String precedingWord, Collection<String[]> set) {
+        return count(sequentWord, precedingWord, set) / count(precedingWord, set);
+    }
 
-  private static Long calculateProbability(String word, Collection<String> set) {
-    return count(word, set) / set.size();
-  }
+    public static Double calculateBigramPriorSmoothingProbability(String sequentWord, String precedingWord, Collection<String[]> set, Double k) {
+        return (count(sequentWord, precedingWord, set) + k * calculateProbability(sequentWord, set)) / (count(precedingWord, set) + k * set.size());
+    }
+
+    private static Double calculateProbability(String word, Collection<String[]> set) {
+        return count(word, set) / set.size();
+    }
 
 
 }
