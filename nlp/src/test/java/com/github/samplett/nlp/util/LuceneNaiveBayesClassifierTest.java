ad7c2649184fb799c299a06841fafd53e7bcaf6e
Tommaso Teofili <tommaso.teofili@gmail.com>
2012-07-02 12:32:31 +0200

    test fixed (new document needs to be created each time)

--- nlp/src/test/java/com/github/samplett/nlp/util/LuceneNaiveBayesClassifierTest.java
+++ nlp/src/test/java/com/github/samplett/nlp/util/LuceneNaiveBayesClassifierTest.java
@@ -37,6 +37,7 @@ public class LuceneNaiveBayesClassifierTest {
         d.add(new Field("class", "A", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "TRASTEVERE via degli Orti di Trastevere in palazzo " +
                 "signorile (con s. portineria) appartamento mq 180 + cantina mq" +
                 " 6 con rifiniture di pregio marmi & armadi a muro + ampia " +
@@ -44,6 +45,7 @@ public class LuceneNaiveBayesClassifierTest {
         d.add(new Field("class", "N", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "CORSO VITTORIO Emanuele V. del banco di santo spirito" +
                 " 3° piano con ascensore appartamento di 142 mq commerciali " +
                 "composto da: ingresso disimpegno tre camere soggiorno cucina" +
@@ -51,6 +53,7 @@ public class LuceneNaiveBayesClassifierTest {
         d.add(new Field("class", "A", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "TRASTEVERE Ippolito Nievo quinto piano tripla " +
                 "esposizione ingresso salone doppio cucina abitabile tre " +
                 "camere servizio ripostiglio terrazzo e soffitta da ristrutturare " +
@@ -58,12 +61,14 @@ public class LuceneNaiveBayesClassifierTest {
         d.add(new Field("class", "A", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "TRASTEVERE E.Rolli solo privati palazzo epoca doppia" +
                 " esposizione ingresso soppalcato soggiorno 2 camere cucinotto " +
                 "bagno 84 mq IV piano no ascensore 385.000 giardino condominio", type));
         d.add(new Field("class", "N", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "CENTRO monti sforza elegante edificio con ampi spazi" +
                 " comuni ristrutturato ingresso soggiorno angolo cucina camera " +
                 "letto armadi a muro bagno vasca con finestra pavimenti cotto " +
@@ -72,17 +77,20 @@ public class LuceneNaiveBayesClassifierTest {
         d.add(new Field("class", "A", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "SAN LORENZO app.to epoca privato vende salone due " +
                 "camere cucina abit. due bagni ripostigli vari II piano con" +
                 " ascensore triplo affaccio E 530.000 ", type));
         d.add(new Field("class", "N", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "SAN LORENZO Via Porta Labicana appartamento mq 80 " +
                 "piano rialzato con ingresso 3 camere cucina bagno E 395.000 ", type));
         d.add(new Field("class", "N", type));
         indexWriter.addDocument(d);
 
+        d = new Document();
         d.add(new Field("text", "SAN LORENZO via degli Umbri I° p. 3 stanze cucina " +
                 "servizio terrazzino interno buono stato E. 390.000 tratt. " +
                 "assoloutamente no agenzie ", type));
