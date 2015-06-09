c2866417144a4ecff5126f0cf3e97f994dbf9b84
Tommaso Teofili <tommaso.teofili@gmail.com>
2015-06-09 12:13:40 +0200

    minor code optimizations

--- skaluh/src/test/scala/com/github/samplett/skaluh/RecommendTest.scala
+++ skaluh/src/test/scala/com/github/samplett/skaluh/RecommendTest.scala
@@ -10,43 +10,43 @@ class RecommendTest {
 
   @Test
   def sim_distanceTest() {
-    val r = new Recommend();
+    val r = new Recommend()
     var simDistance = r.sim_distance(r.sampleSet, "paula", "john")
     Console.println("paula and john distance based similarity is : " + simDistance)
     // john and paula are quite far
-    assertTrue(simDistance < 0.1);
+    assertTrue(simDistance < 0.1)
 
     // paula and timothy are less far
     simDistance = r.sim_distance(r.sampleSet, "paula", "timothy")
     Console.println("paula and timothy distance based similarity is : " + simDistance)
-    assertTrue(simDistance > 0.2);
+    assertTrue(simDistance > 0.2)
   }
 
   @Test
   def sim_pearsonTest() {
-    val r = new Recommend();
+    val r = new Recommend()
     var simPearson = r.sim_pearson(r.sampleSet, "paula", "john")
     Console.println("paula and john pearson correlation based similarity is : " + simPearson)
     // john and paula are quite far
-    assertTrue(simPearson < 0.1);
+    assertTrue(simPearson < 0.1)
 
     // paula and timothy are less far
     simPearson = r.sim_pearson(r.sampleSet, "paula", "timothy")
     Console.println("paula and timothy pearson correlation based similarity is : " + simPearson)
-    assertTrue(simPearson > 0.2);
+    assertTrue(simPearson > 0.2)
   }
 
   @Test
   def topMatchesTestWithDistance() {
-    val r = new Recommend();
-    var sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_distance)
+    val r = new Recommend()
+    val sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_distance)
     Console.println(sortedList)
   }
 
   @Test
   def topMatchesTestWithPearson() {
-    val r = new Recommend();
-    var sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_pearson)
+    val r = new Recommend()
+    val sortedList = r.topMatches(r.sampleSet, "paula", 2, r.sim_pearson)
     Console.println(sortedList)
   }
 
