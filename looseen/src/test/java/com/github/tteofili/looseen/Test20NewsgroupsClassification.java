fa56789a086266eacffd8f0692efc68fb7f3939e
Tommaso Teofili <teofili@adobe.com>
2016-04-07 14:49:15 +0200

    removed useless annotations

--- looseen/src/test/java/com/github/tteofili/looseen/Test20NewsgroupsClassification.java
+++ looseen/src/test/java/com/github/tteofili/looseen/Test20NewsgroupsClassification.java
@@ -75,8 +75,6 @@ import org.apache.lucene.util.TimeUnits;
 import org.junit.Test;
 
 @LuceneTestCase.SuppressSysoutChecks(bugUrl = "none")
-//@TimeoutSuite(millis = 365 * 24 * TimeUnits.HOUR) // hopefully ~1 year is long enough ;)
-//@LuceneTestCase.Monster("takes a lot!")
 public final class Test20NewsgroupsClassification extends LuceneTestCase {
 
     private static final String PREFIX = "/Users/teofili/data";
